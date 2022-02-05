dd21397f6cd089efdde281af8d5cf54fefd54069
Combind <webmaster@combind.ma>
2022-02-05 16:07:40 +0100

    wip

--- src/helpers.php
+++ src/helpers.php
@@ -1,11 +1,5 @@
 <?php
 
-if (! function_exists('url_image')) {
-    function url_image(string $link, string $options = 't_default')
-    {
-        return 'https://res.cloudinary.com/tasmim/image/upload/' . $options . '/' . $link;
-    }
-}
 if (! function_exists('truncate')) {
     function truncate($string, $length = 100, $append = "...")
     {
@@ -18,20 +12,6 @@ if (! function_exists('truncate')) {
         }
 
         return $string;
-    }
-}
-
-
-if (! function_exists('readableSize')) {
-    function readableSize($bytes)
-    {
-        $units = ['B', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB'];
-
-        for ($i = 0; $bytes > 1024; $i++) {
-            $bytes /= 1024;
-        }
-
-        return round($bytes, 2) . ' ' . $units[$i];
     }
 }
 
